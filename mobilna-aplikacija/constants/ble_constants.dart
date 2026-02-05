26ef3378a7384f6713eac161d6610d7936ffc252
bjelinic-2001 <bjelinic@tvz.hr>
2026-02-05 21:27:11 +0100

    Add files via upload

---
+++ mobilna-aplikacija/constants/ble_constants.dart
@@ -0,0 +1,4 @@
+const String DEVICE_NAME = "ESP32-CarKey";
+const String SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+const String TX_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+const String RX_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
